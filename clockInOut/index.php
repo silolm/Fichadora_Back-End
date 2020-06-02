6b11b5a07010d8cc6c82048334de7499825162c8
Mario Silla Olmos <silolm.mail@gmail.com>
2020-06-02 19:54:04 +0200

    add Functions GET POST & PUT

--- clockInOut/index.php
+++ clockInOut/index.php
@@ -1,15 +1,48 @@
 <?php
 include '../utils/db.php';
 
-$keep =  dbReq('SELECT * FROM clockinouts', function ($row) {
-    return ([
-        "id" => $row[0],
-        "employee" => $row[1],
-        "in" => $row[2],
-        "out" => $row[3],
-        "pauseIn" => $row[4],
-        "pauseOut" => $row[5]
-    ]);
-});
+function get()
+{
+    $keep = dbQuery('SELECT * FROM clockinouts', function ($row) {
+        return ([
+            "id" => $row[0],
+            "employee" => $row[1],
+            "in" => $row[2],
+            "out" => $row[3],
+            "pauseIn" => $row[4],
+            "pauseOut" => $row[5]
+        ]);
+    });
+    echo json_encode($keep);
+}
 
-echo json_encode($keep);
+function post()
+{
+    $body = json_decode(file_get_contents("php://input"), true);
+    if (!empty($body)) {
+        $id = dbQuery("INSERT INTO clockinouts (`id`, `employee`, `in`, `out`, `pauseIn`, `pauseOut`) VALUES (NULL, '"
+            . $body['employee'] . "','" . $body['in'] . "','" . $body['out'] . "','" . $body['pauseIn'] . "','" . $body['pauseOut'] . "')");
+
+        echo json_encode(['id' => $id]);
+    }
+}
+
+function put()
+{
+    $body = json_decode(file_get_contents("php://input"), true);
+    if (!empty($body))
+        dbQuery("UPDATE clockinouts SET `out` = '" . $body['out'] . "'," . "pauseIn" . " = '" . $body['pauseIn'] . "'," . "pauseOut" . " = '" . $body['pauseOut'] . "' WHERE id LIKE " . $body['id']);
+}
+
+$method = $_SERVER['REQUEST_METHOD'];
+switch ($method) {
+    case "GET":
+        get();
+        break;
+    case "POST":
+        post();
+        break;
+    case "PUT":
+        put();
+
+}
--- utils/db.php
+++ utils/db.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 $db = "backend";
 
-function dbReq($query, $callback)
+function dbQuery($query, $callback = null)
 {
     # Crear conexión
     $conn = mysqli_connect($GLOBALS["host"], $GLOBALS["username"], $GLOBALS["password"], $GLOBALS["db"]);
@@ -12,12 +12,15 @@ function dbReq($query, $callback)
     if (!$conn)
         die("Conexi&ocacuten fallida: " . mysqli_connect_error());
 
-    $result = mysqli_query($conn, $query);
+    $result = mysqli_query($conn, $query) or trigger_error("Query Failed! SQL: $query - Error: " . mysqli_error($conn), E_USER_ERROR);
     $warehouse = [];
 
-    while ($row = mysqli_fetch_array($result)) {
-        array_push($warehouse, $callback($row));
-    }
+    if ($callback !== null) {
+        while ($row = mysqli_fetch_array($result)) {
+            array_push($warehouse, $callback($row));
+        }
+    } else return $conn->insert_id;
+
 
     mysqli_free_result($result);
     mysqli_close($conn);
