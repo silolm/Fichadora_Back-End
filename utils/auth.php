3cd5c4c04c4195f28b9db148f9537ce6167fed46
Mario Silla Olmos <silolm.mail@gmail.com>
2020-06-17 02:36:09 +0200

    fixes

--- utils/auth.php
+++ utils/auth.php
@@ -47,7 +47,7 @@ function selectAction($actions, $callback)
                 $data = json_decode(file_get_contents("php://input"), true); 
                 if (hasAuth(Permisions::write, $callback, $data)) {
                     $result = $actions['POST']($data);
-                    header("HTTP/1.1 204 No Content");
+                    header("HTTP/1.1 200 No Content");
                     echo $result;
                     return;
                 }
